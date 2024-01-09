86587c4720203c2758c532c04f5956f629c37275
Karim Mohamed <komicho1996@gmail.com>
2024-01-09 22:53:59 +0200

    Refactor: add providers to the `composer.json` file

--- obelaw.php
+++ obelaw.php
@@ -1,8 +1,5 @@
 <?php
 
 use Obelaw\Schema\BundleRegistrar;
-use Obelaw\Permissions\ObelawPermissionsServiceProvider;
 
-BundleRegistrar::register(BundleRegistrar::MODULE, 'obelaw_helper_permissions', __DIR__, function ($config) {
-    $config->setProvider(ObelawPermissionsServiceProvider::class);
-});
+BundleRegistrar::register(BundleRegistrar::MODULE, 'obelaw_helper_permissions', __DIR__);
